b8c83c8e42e04128e624261718375d60e3511949
kshitijofficial <52281964+kshitijofficial@users.noreply.github.com>
2022-06-27 20:44:15 +0530

    Update Calculator.sol

--- contracts/Calculator.sol
+++ contracts/Calculator.sol
@@ -2,13 +2,6 @@
 pragma solidity >=0.5.0 <0.9.0;
 
 abstract contract Calculator {
-   function sum() public view virtual returns(uint);
+  
 }
-contract Test is Calculator {
-   function sum() public pure override returns(uint) {
-      uint a = 1;
-      uint b = 2;
-      uint result = a + b;
-      return result;
-   }
-}
+//Create Test contract
